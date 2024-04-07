6ac05b986a7d7c88b3961f71cf1bdb69977a070a
Garrett Downs <garredow@gmail.com>
2024-04-07 02:07:38 -0400

    refactor

--- src/renderer/src/lib/utilities/storage.ts
+++ src/renderer/src/lib/utilities/storage.ts
@@ -1,13 +1,13 @@
 export enum StorageKey {
-  Settings = 'settings',
-  StoreDb = 'storeDb',
+	Settings = 'settings',
+	StoreDb = 'storeDb'
 }
 
 export function getStorageItem<T>(key: StorageKey): T {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
+	const data = localStorage.getItem(key);
+	return data ? JSON.parse(data) : null;
 }
 
 export function setStorageItem<T>(key: StorageKey, value: T): void {
-  localStorage.setItem(key, JSON.stringify(value));
+	localStorage.setItem(key, JSON.stringify(value));
 }
