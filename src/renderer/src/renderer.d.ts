4ad69a82cfc5e6769ce4b6f54f54b4baf06bdaa0
Garrett Downs <garredow@gmail.com>
2024-04-08 01:21:04 -0400

    fix types

--- src/renderer/src/renderer.d.ts
+++ src/renderer/src/renderer.d.ts
@@ -2,4 +2,18 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-icons/types/svelte" />
 
+import { api } from '$preload/preload';
+
+declare global {
+	interface Window {
+		// see preload.ts for implementation
+		electron: {
+			ipcRenderer: IpcRenderer;
+			webFrame: WebFrame;
+			process: NodeProcess;
+		};
+		api: typeof api;
+	}
+}
+
 export {};
